240c43f429f3ba5f0eafab81785b1a612ad1486b
TessaMay25 <tessamay25@marlborough.org>
2024-09-18 12:48:36 -0700

    Unit1

---
+++ unit0/Geometric.java
@@ -0,0 +1,13 @@
+public class Geometric
+{
+    public static void main(String[] args)
+    {
+        System.out.println();
+        for (int i=0; i<50; i++)
+        {
+            if (i%8 == 0)
+                System.out.println(i);
+            
+        }
+    }
+}
---
+++ unit0/MultiplesOf3.java
@@ -0,0 +1,10 @@
+public class MultiplesOf3
+{
+    public static void main(String[] args)
+    {
+        System.out.println();
+        
+        for (int i=0; i<31; i+=3)
+            System.out.println(i);
+    }
+}
